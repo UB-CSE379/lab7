7660758a7f3d4cca3b6cf3890a33c3cb59fb73cf
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-04-10 15:04:39 -0400

    Combined alice max timer setup in main

--- library_lab_7.s
+++ library_lab_7.s
@@ -3,7 +3,7 @@
 	.global board
 	.global current
 	.global pauseflag
-
+	.global maxTime
 
 	.text
 	.global uart_init
@@ -17,15 +17,19 @@
 	.global gpio_interrupt_init
 	.global uart_interrupt_init
 	.global timer_interrupt_init
+	.global gpio_btn_and_LED_init
 	.global simple_read_character
 
-;_______________________________________________________________________________________
+	.global read_from_push_btns
 
+;_______________________________________________________________________________________
+maxTime:		.word 0
 
 U0FR: 		.equ 0x18
 
 ptr_to_current:			.word current
 ptr_to_pauseflag:		.word pauseflag
+ptr_to_maxTime:			.word maxTime
 
 ;_______________________________________________________________________________________
 
@@ -175,7 +179,6 @@ uart_init:
     STR r1, [r0]
 
 
-
     MOV r0, #0xC028
     MOVT r0, #0x4000
     MOV r1, #44
@@ -231,6 +234,134 @@ uart_init:
 	MOV pc, lr
 
 ;_______________________________________________________________________________________
+
+
+gpio_btn_and_LED_init:
+	PUSH {r4-r12,lr}	; Spill registers to stack
+
+	;Enable Clocks for Ports B, D, and F
+	MOV r4, #0xE000
+	MOVT r4, #0x400F
+	LDRB r5, [r4, #0x608]		  ;00  1  0  1  0  1  0
+	ORR r5, r5, #0x2A ;0010 1010  Port F, E, D, C, B, A
+	STRB r5, [r4, #0x608]
+
+	;Port F Pin 4 is input, write 0
+		; - need a pull-up resistor for SW1
+	; Port D Pins 0-3 are input for Btns, write 0
+	; Port B Pins 0-3 are output for LEDs, write 1
+	; Port B Base Address-> 0x40005000
+	MOV r6, #0x5000
+	MOVT r6, #0x4000
+	;Port D Base Address -> 0x40007000
+	MOV r7, #0x7000
+	MOVT r7, #0x4000
+
+	;Port F Base Address -> 0x40025000
+	MOV r8, #0x5000
+	MOVT r8, #0x4002
+
+	;Set Pin Directions
+	;Port B Pin Direction is Output, write 1 to pins 0 - 3
+	LDRB r9, [r6, #0x400]
+    ORR r9, r9, #0x0F ;configure pins 0 - 3 as output
+    STRB r9, [r6, #0x400] ; write 1 to mem
+
+	;Port D Pin Direction is Input, Write 0 to pins 0-3
+	LDRB r9, [r7, #0x400]
+	AND r9, r9, #0x00
+	STRB r9, [r7, #0x400]
+
+	;Port F Pin Direction is Input, Write 0 to pin 4
+	; Port F Pin Direction Output for pins 0 - 3, write 1
+	LDRB r9, [r8, #0x400]
+	ORR r9, r9, #0x0E ; 0000 1110
+	STRB r9, [r8, #0x400]
+
+    ;SET PIN AS DIGITAL
+    ; Set Pins 0-3 in Port B Digital, write 1
+    LDRB r10, [r6, #0x51C]
+    ORR r10, r10, #0x0F
+    STRB r10, [r6, #0x51C]
+
+    ; Set Pins 0-3 in Port D Digital, write 1
+    LDRB r11, [r7, #0x51C]
+    ORR r11, r11, #0x0F
+    STRB r11, [r7, #0x51C]
+
+    ; Initilize Pull-up resistor for Port F, write 1
+    LDRB r12, [r8, #0x510]
+    ORR r12, r12, #0x10
+    STRB r12, [r8, #0x510]
+
+    ; Set Pin pins 1-4 in Port F Digital, write 1
+    LDRB r12, [r8, #0x51C]
+    ORR r12, r12, #0x1E
+    STRB r12, [r8, #0x51C]
+
+          ; Your code is placed here
+
+	POP {r4-r12,lr}  	; Restore registers from stack
+	MOV pc, lr
+;_________________________________________________________________________________________________________________
+
+read_from_push_btns:
+	PUSH {r4-r12,lr}	; Spill registers to stack
+
+    ; Your code is placed here
+    ;Initilize r3 with PORT D Base Address
+    MOV r3, #0x7000
+    MOVT r3, #0x4000
+
+	LDR r4, ptr_to_maxTime
+
+LOOP20:
+ 	;GPIODATA
+ 	LDRB r9, [r3, #0x3FC]
+ 	AND r9, r9, #0x0F ; if r9 == 0000 0001 SW5 is pressed
+ 					  ; if r9 == 0000 0010 SW4 is pressed
+ 					  ; if r9 == 0000 0100 SW3 is pressed
+ 					  ; if r9 == 0000 1000 SW2 is pressed
+
+ 	;Find which button was pressed
+ 	CMP r9, #0x01 ; SW5 is pressed
+ 	BEQ PRESS_5
+
+ 	CMP r9, #0x02; SW4 is pressed
+ 	BEQ PRESS_4
+
+ 	CMP r9, #0x04; SW3 is pressed
+ 	BEQ PRESS_3
+
+ 	CMP r9, #0x08; SW2 is pressed
+ 	BEQ PRESS_2
+
+ 	B LOOP20
+
+PRESS_5: ;100 seconds for sw5
+	MOV r6, #100
+	STR r6, [r4]
+	B STOP_BTNS
+
+PRESS_4: 	;200 seconds limit for sw4
+	MOV r6, #200
+	STR r6, [r4]
+	B STOP_BTNS
+
+PRESS_3:	;300 seconds limit for sw3
+	MOV r6, #300
+	STR r6, [r4]
+	B STOP_BTNS
+PRESS_2:	; unlimited limit for sw2, FIX LATER
+	MOV r6, #-1
+	STR r6, [r4]
+	B STOP_BTNS
+
+STOP_BTNS:
+	POP {r4-r12,lr}  	; Restore registers from stack
+	MOV pc, lr
+
+;_________________________________________________________________________________________________________________________________________________
 
 output_character:
 	PUSH {r4-r12,lr}	; Spill registers to stack
