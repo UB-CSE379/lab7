ade96ae828afa2795dc0d271e3daac9b5422e406
Gauravjit Chhina <112595926+Gary9528@users.noreply.github.com>
2024-04-10 23:31:44 -0400

    Update lab7.s

--- lab7.s
+++ lab7.s
@@ -3,125 +3,77 @@
 	.global board
 	.global current
 	.global pauseflag
-	.global maxTime
 
 testinglookuptable: .string "123456789",0
 
 
 
 board:	.string "+--------------------+", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
 		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m  ",27,"[40m  ",27,"[43m  ",27,"[40m","|", 27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
 		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
+		.string "|      |      |      |", 0xA, 0xD
 		.string "+--------------------+", 0
+		
 
-board2:	.string "+--------------------+", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "+--------------------+", 0
+scoreline:		.string 27,"[15;1H",0
+clearline:		.string 27,"[K",0
 
-board3:	.string "+--------------------+", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "+--------------------+", 0
+b1sqr1_1:		.string 27,"[2;2H      "
+b1sqr1_2:		.string 27,"[3;2H      "
+b1sqr1_3:		.string 27,"[4;2H      "
+				.string 27,"[40m",0
 
-board4:	.string "+--------------------+", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "+--------------------+", 0
+b1sqr2_1:		.string 27,"[6;2H      "
+b1sqr2_2:		.string 27,"[7;2H      "
+b1sqr2_3:		.string 27,"[8;2H      "
+				.string 27,"[40m",0
 
-board5:	.string "+--------------------+", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "+--------------------+", 0
-
-board6:	.string "+--------------------+", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|--------------------|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "|",27,"[44m      ",27,"[40m","|",27,"[43m      ",27,"[40m","|",27,"[41m      ",27,"[40m","|", 0xA, 0xD
-		.string "+--------------------+", 0
+b1sqr3_1:		.string 27,"[10;2H      "
+b1sqr3_2:		.string 27,"[11;2H      "
+b1sqr3_3:		.string 27,"[12;2H      "
+				.string 27,"[40m",0
 
 
-select_time_prompt:
-					.string "", 0xA, 0xD
-					.string "Select one of the Momementary Buttons on the Alice Board for a time limit", 0xA, 0xD
-					.string "Press SW5 - 100 second limit", 0xA, 0xD
-					.string "Press SW4 - 200 second limit", 0xA, 0xD
-					.string "Press SW3 - 300 second limit", 0xA, 0xD
-					.string "Press SW2 - No time limit", 0xA, 0xD
+redtile: 	.string 27,"[41m",0
+bluetile: 	.string 27,"[44m",0
+whitetile:  .string 27,"[47m",0
+greentile:  .string 27,"[42m",0
+yellowtile: .string 27,"[43m",0
+purpletile: .string 27,"[45m",0
+resettile: 	.string 27,"[40m",0
 
-prompt1: 			.string "", 0xA, 0xD
-					.string "Please Press SW1 On the Tiva To Play!!!", 0xA, 0xD
+
+paused: 			.string "", 0xA, 0xD
+					.string "Game Paused!!!", 0xA, 0xD
 					.string "",0x0
 
-lastprompt: 		.string "", 0xA, 0xD
-					.string "GAME OVER!!!", 0xA, 0xD
-					.string "Your Score is: ",0
 
 clearscreen: 		.string 27,"[2J",0
 leftside: 			.string 27,"[0;0H",0
 
-scoreOutput: 		.string "                                h"
+
+
 newLine: 			.cstring "\r\n"
-score: 				.word 1
-runningscore:	.cstring "Score: "
+
+timeOutput: 		.string "                                h"
+time: 				.word 0
+runningtime:	.cstring "time: "
+
+moveOutput: 		.string "                                h"
+moves: 				.word 0
+runningmoves:		.cstring "Moves: "
 
 
-current:		.string "w",0
+current:		.string "x",0
 position: 		.word 0xFB	; center
-check3:			.word 0
 pauseflag:		.word 0
 
 
@@ -144,7 +96,6 @@ pauseflag:		.word 0
 	.global output_string			; This is from your Lab #4 Library
 	.global uart_init				; This is from your Lab #4 Library
 	.global int2string				; This is from your Lab #4 Library
-	.global read_from_push_btns
 
 ;_______________________________________________________________________________________
 
@@ -155,25 +106,50 @@ U0FR: 		.equ 0x18
 
 ptr_to_board: 			.word board
 ptr_to_clearscreen:		.word clearscreen
-ptr_to_leftside:		.word leftside
+ptr_to_leftside			.word leftside
 
-ptr_to_alice_prompt:	.word select_time_prompt
-ptr_to_prompt1: 		.word prompt1
-ptr_to_lastprompt: 		.word lastprompt
+ptr_to_paused: 			.word paused
 
-ptr_to_runningscore: 	.word runningscore
-ptr_scoreOutput: 		.word scoreOutput
+
+ptr_to_time:			.word time
+ptr_to_runningtime: 	.word runningtime
+ptr_timeOutput: 		.word timeOutput
+
+ptr_to_moves:			.word moves
+ptr_to_runningmoves: 	.word runningmoves
+ptr_moveOutput: 		.word moveOutput
+
+
 ptr_newLine: 			.word newLine
 
 ptr_to_current:			.word current
 ptr_position:			.word position
 
-ptr_to_check3:			.word check3
+
 ptr_to_pauseflag:		.word pauseflag
-ptr_to_maxTime:			.word maxTime
-ptr_score:				.word score
 
+ptr_to_redtile:			.word redtile
+ptr_to_bluetile:		.word bluetile
+ptr_to_greentile:		.word greentile
+ptr_to_whitetile:		.word whitetile
+ptr_to_purpletile:		.word purpletile
+ptr_to_yellowtile:		.word yellowtile
+ptr_to_resettile:		.word resettile
+ptr_to_clearline:		.word clearline
 
+ptr_to_b1sqr1_1:		.word b1sqr1_1
+ptr_to_b1sqr1_2:		.word b1sqr1_2
+ptr_to_b1sqr1_3:		.word b1sqr1_3
+
+ptr_to_b1sqr2_1:		.word b1sqr2_1
+ptr_to_b1sqr2_2:		.word b1sqr2_2
+ptr_to_b1sqr2_3:		.word b1sqr2_3
+
+ptr_to_b1sqr3_1:		.word b1sqr3_1
+ptr_to_b1sqr3_2:		.word b1sqr3_2
+ptr_to_b1sqr3_3:		.word b1sqr3_3
+
+ptr_to_scoreline:		.word scoreline
 ;_______________________________________________________________________________________
 
 lab7:
@@ -186,11 +162,6 @@ lab7:
 	BL timer_interrupt_init
 
 
-	LDR r0, ptr_to_alice_prompt ;Prompt to tell user to select an Alice button for how long they want the game
-	BL output_string
-
-	BL read_from_push_btns ; will loop till one of the buttons pressed, max time limit is set
-
 	LDR r0, ptr_to_clearscreen	; clears the uart
 	BL output_string
 
@@ -199,6 +170,28 @@ lab7:
 
 	LDR r0, ptr_to_board			;prints the initial board
     BL output_string
+
+
+	LDR r0, ptr_to_redtile
+	BL output_string
+
+	LDR r0, ptr_to_b1sqr1_1
+	BL output_string
+
+	LDR r0, ptr_to_yellowtile
+	BL output_string
+
+	LDR r0, ptr_to_b1sqr2_1
+	BL output_string
+
+	LDR r0, ptr_to_bluetile
+	BL output_string
+
+	LDR r0, ptr_to_b1sqr3_1
+	BL output_string
+
+
+
 
     B end
 
@@ -217,16 +210,16 @@ lab6GameEnd:
 	BL output_string
 
 	;outputs score during each tick
-	LDR r0, ptr_to_runningscore
+	LDR r0, ptr_to_runningtime
 	BL output_string
 
 
-	LDR r0, ptr_scoreOutput
-	LDR r2, ptr_score
+	LDR r0, ptr_timeOutput
+	LDR r2, ptr_to_time
 	LDR r1, [r2]
 	BL int2string
 
-	LDR r0, ptr_scoreOutput
+	LDR r0, ptr_timeOutput
 	BL output_string
 	LDR r0, ptr_newLine
 	BL output_string
@@ -234,9 +227,30 @@ lab6GameEnd:
 	LDR r0, ptr_to_board			;prints the board
     BL output_string
 
-
-	LDR r0, ptr_scoreOutput
+    LDR r0, ptr_newLine
 	BL output_string
+
+	LDR r0, ptr_to_runningmoves
+	BL output_string
+
+
+	LDR r0, ptr_moveOutput
+	LDR r2, ptr_to_moves
+	LDR r1, [r2]
+	BL int2string
+
+	LDR r0, ptr_moveOutput
+	BL output_string
+	LDR r0, ptr_newLine
+	BL output_string
+
+
+	LDR r0, ptr_timeOutput
+	BL output_string
+
+
+
+
 	B end
 
 
@@ -260,13 +274,9 @@ Timer_Handler:
 	LDR r0, ptr_to_pauseflag
 	LDR r1, [r0]
 	CMP r1, #1
-	BEQ timerEnd
+	BEQ PAUSE
 
-								; check check3
-	LDR r0, ptr_to_check3
-	LDR r1, [r0]
-	CMP r1, #1
-	BEQ timerEnd
+
 
 
 							; prev keys
@@ -278,34 +288,21 @@ Timer_Handler:
 	BEQ dletter
 	CMP r1, #'a'
 	BEQ aletter
+	CMP r1, #'w'
+	BEQ wletter
+
+	BNE mdone
 
 
 wletter:
 	LDR r0, ptr_position
 	LDR r1, [r0]
-	SUB r1, r1, #24
 
-	LDR r2, ptr_to_board
-	LDRB r3, [r2, r1]
-
-	CMP r3, #'*'
-	BEQ lcheck
-	CMP r3, #'|'
-	BEQ lcheck
-	CMP r3, #'-'
-	BEQ lcheck
-
-	LDR r0, ptr_position
+	LDR r0, ptr_to_moves	;increases the moves by 1
 	LDR r1, [r0]
-	SUB r1, r1, #24
-	MOV r3, r1
+	ADD r1, r1, #1
 	STR r1, [r0]
 
-
-
-	LDR r0, ptr_to_board	;update on the board
-	MOV r4, #'*'
-	STRB r4, [r0, r3]
 
 	B mdone
 
@@ -315,32 +312,12 @@ aletter:
 
 	LDR r0, ptr_position
 	LDR r1, [r0]
-	SUB r1, r1, #1
 
-
-	LDR r2, ptr_to_board		;check
-	LDRB r3, [r2, r1]
-
-	CMP r3, #'*'
-	BEQ lcheck
-	CMP r3, #'|'
-	BEQ lcheck
-	CMP r3, #'-'
-	BEQ lcheck
-
-
-
-	LDR r0, ptr_position		;update in buffer
+	LDR r0, ptr_to_moves	;increases the moves by 1
 	LDR r1, [r0]
-	SUB r1, r1, #1
-	MOV r3, r1
+	ADD r1, r1, #1
 	STR r1, [r0]
 
-
-
-	LDR r0, ptr_to_board		;update on board
-	MOV r4, #'*'
-	STRB r4, [r0, r3]
 
 	B mdone
 
@@ -349,31 +326,11 @@ sletter:
 
 	LDR r0, ptr_position
 	LDR r1, [r0]
-	ADD r1, r1, #24
 
-
-	LDR r2, ptr_to_board		;check
-	LDRB r3, [r2, r1]
-
-	CMP r3, #'*'
-	BEQ lcheck
-	CMP r3, #'|'
-	BEQ lcheck
-	CMP r3, #'-'
-	BEQ lcheck
-
-
-	LDR r0, ptr_position	;update in buffer
+	LDR r0, ptr_to_moves	;increases the moves by 1
 	LDR r1, [r0]
-	ADD r1, r1, #24
-	MOV r3, r1
+	ADD r1, r1, #1
 	STR r1, [r0]
-
-
-
-	LDR r0, ptr_to_board	;update on board
-	MOV r4, #'*'
-	STRB r4, [r0, r3]
 
 	B mdone
 
@@ -381,30 +338,12 @@ dletter:
 
 	LDR r0, ptr_position
 	LDR r1, [r0]
-	ADD r1, r1, #1
 
-
-	LDR r2, ptr_to_board	;check
-	LDRB r3, [r2, r1]
-
-	CMP r3, #'*'
-	BEQ lcheck
-	CMP r3, #'|'
-	BEQ lcheck
-	CMP r3, #'-'
-	BEQ lcheck
-
-
-	LDR r0, ptr_position	;updates in buffer
+	LDR r0, ptr_to_moves	;increases the moves by 1
 	LDR r1, [r0]
 	ADD r1, r1, #1
-	MOV r3, r1
 	STR r1, [r0]
 
-
-	LDR r0, ptr_to_board	;updates on board
-	MOV r4, #'*'
-	STRB r4, [r0, r3]
 
 	B mdone
 
@@ -412,21 +351,19 @@ dletter:
 mdone:
 
 
-	LDR r0, ptr_score	;increases the score by 1
+	LDR r0, ptr_to_time	;increases the score by 1
 	LDR r1, [r0]
 	ADD r1, r1, #1
 	STR r1, [r0]
 	B outputFunction
 
-lcheck:
-	LDR r0, ptr_to_check3
-	MOV r1, #1
-	STR r1, [r0]
-	B lout
 
 outputFunction:
 
-	LDR r0, ptr_to_clearscreen	; clears the uart
+	LDR r0, ptr_to_scoreline
+	BL output_string
+
+	LDR r0, ptr_to_clearline
 	BL output_string
 
 	LDR r0, ptr_to_leftside		; Goes to left side idk why but from lect
@@ -434,23 +371,41 @@ outputFunction:
 
 
 
-
-	LDR r0, ptr_to_runningscore ;prints score
+	LDR r0, ptr_to_scoreline
 	BL output_string
 
-	LDR r0, ptr_scoreOutput
-	LDR r2, ptr_score
+	LDR r0, ptr_to_runningtime ;prints score
+	BL output_string
+
+	LDR r0, ptr_timeOutput
+	LDR r2, ptr_to_time
 	LDR r1, [r2]
 	BL int2string
 
-	LDR r0, ptr_scoreOutput
-	BL output_string
-	LDR r0, ptr_newLine
+	LDR r0, ptr_timeOutput
 	BL output_string
 
 
-	LDR r0, ptr_to_board
-    BL output_string
+
+    LDR r0, ptr_newLine
+	BL output_string
+
+	LDR r0, ptr_to_runningmoves ;prints score
+	BL output_string
+
+	LDR r0, ptr_moveOutput
+	LDR r2, ptr_to_moves
+	LDR r1, [r2]
+	BL int2string
+
+	LDR r0, ptr_moveOutput
+	BL output_string
+
+	LDR r0, ptr_to_current
+	LDR r1, [r0]
+	MOV r1, #'x'
+	STR r1, [r0]
+
 
     B timerEnd
 
@@ -462,22 +417,16 @@ lout:
 	LDR r0, ptr_to_leftside		; Goes to left side idk why but from lect
 	BL output_string
 
-	LDR r0, ptr_to_board
-    BL output_string
-    NOP
-    LDR r0, ptr_to_lastprompt
-    BL output_string
 
 
-	LDR r0, ptr_scoreOutput
-	LDR r2, ptr_score
-	LDR r1, [r2]
-	BL int2string
 
-	LDR r0, ptr_scoreOutput
-	BL output_string
+
 
     B timerEnd
+
+PAUSE:
+	LDR r0, ptr_to_paused
+	BL output_string
 
 timerEnd:
 	POP {r4-r12,lr}
@@ -486,3 +435,4 @@ timerEnd:
 end:
 
 	.end
+
